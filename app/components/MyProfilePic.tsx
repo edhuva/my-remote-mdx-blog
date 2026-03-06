2b23abc7692b344ab40d6c44185568f7eacdf0db
edhuva <edhuva491@gmail.com>
2026-03-06 19:13:38 +0200

    Fixed vulnerable issue

--- app/components/MyProfilePic.tsx
+++ app/components/MyProfilePic.tsx
@@ -6,7 +6,7 @@ export default function MyProfilePic() {
         <Image
         className="border-4 border-black dark:border-slate-500 drop-shadow-xl shadow-black
         rounded-full mx-auto mt-8"
-            src="/images/edhuva.jpg"
+            src="/images/edhuva.webp"
             width={200}
             height={200}
             alt="Edwin Matema"
